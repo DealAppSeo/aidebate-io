30d2d31ce7dc626b5ccbad6b11eed185f557d63c
Sean Goodwin <dealappseo@gmail.com>
2025-12-05 20:16:46 -0800

    Fix Tailwind darkMode config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,10 @@
-import type { Config } from "tailwindcss"
-
-const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        outfit: ['Outfit', 'sans-serif'],
+theme: {
+  extend: {
+    fontFamily: {
+      outfit: ['Outfit', 'sans-serif'],
       },
-    },
   },
+},
 } satisfies Config
 
 export default config
